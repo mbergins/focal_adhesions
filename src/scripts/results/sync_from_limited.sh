64c72770751ab1a03a9595d9d9d8fe2412a0efea
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2011-02-09 15:02:30 +0000

    all: 	-removing old style syncing scripts
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/focal_adhesions/trunk@2722 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/scripts/results/sync_from_limited.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "Syncing Focal Adhesion from $1";
-rsync -a $1:~/Documents/Projects/focal_adhesions/results/focal_adhesions/ ../../results/focal_adhesions;
-echo "Syncing Simulation from $1";
-rsync -a $1:~/Documents/Projects/focal_adhesions/results/simulation/ ../../results/simulation;
