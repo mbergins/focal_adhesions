2476c537393c89a536f5c33284d04fccc8ede826
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2011-02-17 15:37:26 +0000

    sync_to.pl: 	-adding perl based syncing script to FA repo
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/focal_adhesions/trunk@2734 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

---
+++ src/scripts/results/sync_to.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl -w
+
+use Getopt::Long;
+
+my %opt;
+$opt{debug} = 0;
+$opt{server} = "NOSERVER";
+$opt{no_time} = 0;
+$opt{repeat} = 1;
+$opt{delay} = 0;
+GetOptions(\%opt,"server=s", "debug|d", "progress", "no_time|nt", "repeat=s", "delay=s") or die;
+
+my $progress_str = "";
+if ($opt{progress}) {
+    $progress_str = '--progress ';
+}
+
+my $time_str = "time ";
+if ($opt{no_time}) {
+    $time_str = '';
+}
+
+my $command = "$time_str" . "rsync $progress_str" . "-a --exclude data.stor ../../results/* $opt{server}:~/Documents/Projects/focal_adhesions/trunk/results/";
+if ($opt{server} eq "NOSERVER" || $opt{debug}) {
+    print "$command\n";
+} else {
+    while($opt{repeat} != 0) {
+        system "$command";
+        print "Done with sync $opt{repeat}\n\n";
+        $opt{repeat}--;
+        sleep $opt{delay}*60;
+    }
+}
+
