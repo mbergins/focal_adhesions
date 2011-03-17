def6cbfeb60b304133fb23a8cd1178cf2a822763
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2011-03-17 15:57:31 +0000

    bilinear_modeling.R: 	-removing model length writing, pushing back to after all models have been 	produced
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/focal_adhesions/trunk@2783 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/analyze_cell_features/bilinear_modeling.R
+++ src/analyze_cell_features/bilinear_modeling.R
@@ -289,11 +289,6 @@ if (length(args) != 0) {
         model$exp_props = exp_props
         model$exp_dir = data_dir
         model$exp_data = data_set
-        #regexpr returns -1 when no hit is found
-        if (regexpr("Average_adhesion_signal",model_file) != -1) {
-            print('Outputing model assembly and disassembly periods')
-            write_assembly_disassembly_periods(model,data_dir)	
-        }
 
         R_model_file = sub(".csv$", ".Rdata", model_file ,perl=T)
         output_file = file.path(data_dir,'models',R_model_file);
