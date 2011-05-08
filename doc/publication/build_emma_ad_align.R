341380fc8767489b1bfb696dfc58eacf6fee13ee
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2011-05-08 17:18:33 +0000

    emma figures R files: 	-adding into repo
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/focal_adhesions/trunk@2831 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

---
+++ doc/publication/build_emma_ad_align.R
@@ -0,0 +1,293 @@
+rm(list = ls())
+library(lattice)
+library(geneplotter)
+library(Hmisc)
+source('FA_analysis_lib.R')
+source('bilinear_modeling.R')
+source('FA_alignment_search.R')
+
+###############################################################################
+# Results Gathering
+###############################################################################
+
+raw_data_base_dir = '../../results/emma/processed_2stdev/';
+
+#######################################
+# Fibronectin Concentrations
+#######################################
+
+align_files = Sys.glob(file.path(raw_data_base_dir,'Fibro_1ug_trial_2/WT_*/*/FA_orientation.Rdata*'))
+fibro_1ug = load_alignment_props(align_files);
+
+align_files = Sys.glob(file.path(raw_data_base_dir,'Fibro_10ug_trial_2/WT_*/*/FA_orientation.Rdata*'))
+fibro_10ug = load_alignment_props(align_files);
+
+align_files = Sys.glob(file.path(raw_data_base_dir,'Fibro_100ug_trial_2/WT_*/*/FA_orientation.Rdata*'))
+fibro_100ug = load_alignment_props(align_files);
+
+#######################################
+# FAK WT/KD
+#######################################
+
+align_files = Sys.glob(file.path(raw_data_base_dir,'100ug_IA32/FAK*/*/FA_orientation.Rdata*'))
+IA32_FAK = load_alignment_props(align_files);
+
+align_files = Sys.glob(file.path(raw_data_base_dir,'100ug_KD/FAK*/*/FA_orientation.Rdata*'))
+KD_FAK = load_alignment_props(align_files);
+
+#######################################
+# Paxillin WT/KD
+#######################################
+
+align_files = Sys.glob(file.path(raw_data_base_dir,'100ug_IA32/Pax*/*/FA_orientation.Rdata*'))
+IA32_Pax = load_alignment_props(align_files);
+
+align_files = Sys.glob(file.path(raw_data_base_dir,'100ug_KD/Pax*/*/FA_orientation.Rdata*'))
+KD_Pax = load_alignment_props(align_files);
+
+#######################################
+# Vinculin WT/KD
+#######################################
+
+align_files = Sys.glob(file.path(raw_data_base_dir,'100ug_IA32/Vin*/*/FA_orientation.Rdata*'))
+IA32_Vin = load_alignment_props(align_files);
+
+align_files = Sys.glob(file.path(raw_data_base_dir,'100ug_KD/Vin*/*/FA_orientation.Rdata*'))
+KD_Vin = load_alignment_props(align_files);
+
+#######################################
+# Haptotaxis
+#######################################
+
+align_files = Sys.glob(file.path(raw_data_base_dir,'Hapto/WT/Pax*/*/FA_orientation.Rdata'))
+hapto_wt = load_alignment_props(align_files);
+ 
+# align_files = Sys.glob(file.path(raw_data_base_dir,'Hapto/2xKD/Pax*/*/FA_orientation*'))
+# hapto_kd = load_alignment_props(align_files)
+
+system('notify-send "done with R"')
+out_folder = '../../doc/publication/figures/emma/'
+
+###########################################################
+# P-value calculations
+###########################################################
+
+# fibro_1_10_p = determine_mean_p_value(fibro_1ug$best_FAAI,fibro_10ug$best_FAAI)
+# fibro_1_100_p = determine_mean_p_value(fibro_1ug$best_FAAI,fibro_100ug$best_FAAI)
+# fibro_10_100_p = determine_mean_p_value(fibro_10ug$best_FAAI,fibro_100ug$best_FAAI)
+# 
+# FAK_p = determine_mean_p_value(IA32_FAK$best_FAAI,KD_FAK$best_FAAI)
+# Pax_p = determine_mean_p_value(IA32_Pax$best_FAAI,KD_Pax$best_FAAI)
+# Vin_p = determine_mean_p_value(IA32_Vin$best_FAAI,KD_Vin$best_FAAI)
+
+stop()
+###############################################################################
+# Plotting
+###############################################################################
+
+###########################################################
+# FAAI
+###########################################################
+
+#######################################
+# Paxillin in NS on Fibro
+#######################################
+
+svg(file.path(out_folder,'FAAI','fibro_FAAI_temp.svg'),width=4,height=4);
+
+#the xpd=T part allows us to draw lines outside the plotting area, this is
+#needed to get the lines under the lables
+par(bty='n',mar=c(2.7,2.6,2.5,0), mgp=c(1.6,0.5,0),xpd=T)
+
+boxplot_with_points(list(fibro_1ug$best_FAAI, fibro_10ug$best_FAAI, 
+    fibro_100ug$best_FAAI,hapto_wt$best_FAAI,KD_Pax$best_FAAI),
+    ylab='FA Alignment Index',with.p.value=F, names = c('1','10','100','Hapto','KD'),
+    colors=c('black'))
+
+boxplot_with_points(list(fibro_1ug$best_FAAI, fibro_10ug$best_FAAI, fibro_100ug$best_FAAI),
+    ylab='FA Alignment Index',with.p.value=F, names = c('1','10','100'),
+    colors=c('black'))
+
+p_size = par("usr");
+char_size = par("cxy")[2];
+
+mtext('Fibronectin Concentration (\u03BCg/mL)',1,at=mean(p_size[1:2]),line=1.6)
+
+#put in the significance brackets
+plot_signif_bracket(c(1,p_size[4]*0.95),c(2,p_size[4]*0.945), 
+    over_text=paste('p<',fibro_1_10_p$p.value[1],sep=''))
+plot_signif_bracket(c(2,p_size[4]*1),c(3,p_size[4]*0.995), 
+    over_text=paste('p<',fibro_10_100_p$p.value[1],sep=''))
+plot_signif_bracket(c(1,p_size[4]*1.04),c(3,p_size[4]*1.035), 
+    over_text=paste('p<',fibro_1_100_p$p.value[1],sep=''))
+graphics.off()
+
+#######################################
+# Adhesion Tags in NS and 2xKD
+#######################################
+
+#placing the labels on this figure has become fairly complicated
+svg(file.path(out_folder,'FAAI','tagged_ads_100ug_comparisons.svg'),width=7,height=4);
+
+#the xpd=T part allows us to draw lines outside the plotting area, this is
+#needed to get the lines under the lables
+par(bty='n',mar=c(2.6,2.6,0,0), mgp=c(1.6,0.5,0),xpd=T)
+
+#skip drawing axes
+boxplot_with_points(list(IA32_Pax$best_FAAI,KD_Pax$best_FAAI,
+    IA32_FAK$best_FAAI,KD_FAK$best_FAAI,
+    IA32_Vin$best_FAAI,KD_Vin$best_FAAI),
+    ylab='FA Alignment Index',ylim=c(40,80),with.p.value=F, 
+    colors=c('black'),axes=F)
+
+p_size = par("usr");
+#draw the axes
+axis(2)
+
+labs = c(paste('NS (n=',length(IA32_Pax$best_FAAI),')',sep=''),
+    paste('2xKD (n=',length(KD_Pax$best_FAAI),')',sep=''))
+axis(1, labels = labs, at = c(1,2))
+lines(c(1,2),rep(p_size[3]*0.89,2),lwd=3)
+mtext('Pax',1,at=1.5,line=1.6)
+
+labs = c(paste('NS (n=',length(IA32_FAK$best_FAAI),')',sep=''),
+    paste('2xKD (n=',length(KD_FAK$best_FAAI),')',sep=''))
+axis(1, labels = labs, at = c(3,4))
+lines(c(3,4),rep(p_size[3]*0.89,2),lwd=3)
+mtext('FAK',1,at=3.5,line=1.6)
+
+labs = c(paste('NS (n=',length(IA32_Vin$best_FAAI),')',sep=''),
+    paste('2xKD (n=',length(KD_Vin$best_FAAI),')',sep=''))
+axis(1, labels = labs, at = c(5,6))
+lines(c(5,6),rep(p_size[3]*0.89,2),lwd=3)
+mtext('Vin',1,at=5.5,line=1.6)
+
+#put in the significance brackets
+plot_signif_bracket(c(1,p_size[4]*0.97),c(2,p_size[4]*0.965), 
+    over_text=paste('p<',Pax_p$p.value[1],sep=''))
+plot_signif_bracket(c(3,p_size[4]*0.97),c(4,p_size[4]*0.965), 
+    over_text=paste('p<',FAK_p$p.value[1],sep=''))
+plot_signif_bracket(c(5,p_size[4]*0.97),c(6,p_size[4]*0.965), 
+    over_text=paste('p<',Vin_p$p.value[1],sep=''))
+graphics.off()
+
+#######################################
+# Single Adhesion Stability
+#######################################
+
+align_files = Sys.glob(file.path(raw_data_base_dir,'Fibro_100ug_trial_2/WT_*/*/FA_orientation.Rdata'));
+lin_files = Sys.glob(file.path(raw_data_base_dir,'Fibro_100ug_trial_2/WT_*/adhesion_props/single_lin.csv'));
+pdf(file.path(out_folder,'single_adhesions_set_60.pdf'));
+for (i in 1:length(align_files)) {
+    print(align_files[i])
+    
+    source('FA_alignment_search.R')
+    var_name = load(align_files[i])
+    sample_data = get(var_name);
+    sample_data$lineage_data <- read.table(lin_files[i],sep=',',header=T);
+    
+    sample_data_filtered = filter_alignment_data(sample_data);
+
+    par(xpd=T)
+    plot_single_adhesion_orientations(sample_data,
+        dominant.angle=sample_data$best_angle,
+        main=align_files[i],with.tags=T,min.area=60)
+}
+graphics.off()
+
+var_name = load('../../results/emma/processed_2stdev/Fibro_100ug_trial_2/WT_02/adhesion_props/FA_orientation.Rdata')
+sample_data = get(var_name);
+sample_data$lineage_data <- read.table('../../results/emma/processed_2stdev/Fibro_100ug_trial_2/WT_01/adhesion_props/single_lin.csv',sep=',',header=T);
+
+source('FA_alignment_search.R')
+plot_single_adhesion_orientations(sample_data,
+    max.data.points=Inf,dominant.angle=sample_data$best_angle,min.data.points=27)
+
+# svg(file.path(out_folder,'single_adhesions.svg'),height=5,width=5);
+# par(bty='n',mar=c(3.1,3.1,0.5,0.1), mgp=c(2.1,1,0))
+# plot_single_adhesion_orientations(sample_data,min.data.points=40,
+#     max.data.points=Inf,dominant.angle=dom_angle,which.ads=c(701, 193, 4032))
+# graphics.off()
+
+#######################################
+# Sample FAAI plots
+#######################################
+
+var_name = load(as.character(fibro_100ug$align_file[1]))
+high_FAAI_data = get(var_name)
+
+svg(file.path(out_folder,'FAAI','sample_high_FAAI.svg'));
+par(bty='n',mar=c(2.8,2.6,0.5,0), mgp=c(1.6,0.5,0),xpd=T,cex=2,lwd=3)
+breaks = hist(high_FAAI_data$corrected_orientation,axes=F,ylab='Number of Adhesions',
+    xlab='FA Orientation',main='')
+axis(2, lwd=3)
+axis(1,at=seq(-90,90,by=45),lwd=3)
+pl_size = par("usr");
+text(pl_size[2],pl_size[4],paste('FAAI=',sprintf('%.1f',fibro_100ug$best_FAAI[1]),sep=''),pos=2)
+graphics.off()
+
+var_name = load(as.character(fibro_1ug$align_file[8]))
+low_FAAI_data = get(var_name)
+
+svg(file.path(out_folder,'FAAI','sample_low_FAAI.svg'));
+par(bty='n',mar=c(2.6,2.8,0.5,0), mgp=c(1.6,0.5,0),xpd=T,cex=2,lwd=3)
+breaks = hist(low_FAAI_data$corrected_orientation,axes=F,ylab='Number of Adhesions',
+    xlab='FA Orientation',main='')
+axis(2, lwd=3)
+axis(1,at=seq(-90,90,by=45),lwd=3)
+pl_size = par("usr");
+text(pl_size[2],pl_size[4],paste('FAAI=',sprintf('%.1f',fibro_1ug$best_FAAI[8]),sep=''),pos=2)
+graphics.off()
+
+#######################################
+# Sample FA orientation cartoon
+#######################################
+
+# load("../../results/emma/Fibro_100ug/WT_02/adhesion_props/FA_orientation.Rdata"); 
+# angle_search = test_dom_angles(data_set$subseted_data$orientation);
+# best_angle = find_best_alignment_angle(angle_search)
+# corrected_orientation = apply_new_orientation(data_set$subseted_data$orientation,best_angle)
+# 
+# per_image_dom_angle = find_per_image_dom_angle(data_set$mat, min_eccen=3)
+# 
+# deg_45_orientation = apply_new_orientation(data_set$subseted_data$orientation,45)
+# deg_90_orientation = apply_new_orientation(data_set$subseted_data$orientation,90)
+# deg_135_orientation = apply_new_orientation(data_set$subseted_data$orientation,135)
+# deg_179_orientation = apply_new_orientation(data_set$subseted_data$orientation,179)
+# 
+# or_data_cartoon = list(data = rbind(data_set$subseted_data$orientation,
+#     deg_45_orientation,deg_90_orientation,deg_135_orientation),
+#     filenames = c('deg_0_hist.svg','deg_45_hist.svg','deg_90_hist.svg','deg_135_hist.svg'));
+# 
+# for (i in 1:length(or_data_cartoon$filenames)) {
+#     svg(file.path(out_folder,'orientation_cartoon',or_data_cartoon$filenames[i]),pointsize=36)
+#     par(bty='n',mar=c(2.5,2.25,0.0,0.5), mgp=c(1.4,0.5,0))
+#     hist(or_data_cartoon$data[i,],breaks=seq(-90,90,by=10),xlim=c(-90,90),axes=F,main='',
+#         xlab='Adhesion Angle',col='black')
+#     axis(2,lwd=3)
+#     axis(1,at=c(-90,0,90),lwd=3)
+#     if (i < 3) {
+#         text(-90,550,paste('FAAI=',sprintf('%.1f',90-sd(or_data_cartoon$data[i,]))),col='red',pos=4)
+#     } else {
+#         text(90,550,paste('FAAI=',sprintf('%.1f',90-sd(or_data_cartoon$data[i,]))),col='red',pos=2)
+#     }
+#     graphics.off()
+# }
+# 
+# svg(file.path(out_folder,'orientation_cartoon','full_FAAI_plot.svg'),height=4,width=14,pointsize=24)
+# 
+# par(bty='n',mar=c(2.5,2.25,0.8,0.5), mgp=c(1.4,0.5,0))
+# plot(angle_search$test_angles,angle_search$angle_FAAI,typ='l',lwd=3,xlab='Alignment Angles (degrees)',ylab='Alignment Index',axes=F)
+# axis(2,lwd=3)
+# axis(1,lwd=3,at=seq(0,180,by=20))
+# 
+# highlight_points = c(0,45,90,135);
+# for (angle in highlight_points) {
+#     degree_index = which(angle_search$test_angles == angle)
+#     points(angle_search$test_angles[degree_index],
+#         angle_search$angle_FAAI[degree_index],col='red',pch=19)
+# }
+# 
+# points(best_angle, 90-sd(corrected_orientation),col='green',pch=19)
+# 
+# graphics.off()
---
+++ doc/publication/build_emma_figures.R
@@ -0,0 +1,357 @@
+rm(list = ls())
+start_time = proc.time()
+library(lattice)
+library(geneplotter)
+library(Hmisc)
+source('FA_analysis_lib.R')
+source('bilinear_modeling.R')
+source('FA_alignment_search.R')
+
+################################################################################
+#Result loading
+################################################################################
+raw_data <- list()
+raw_data_base_dir = '../../results/emma/processed_2stdev/';
+
+########################################
+# Various adhesion tags, all on 100ug fibronectin
+########################################
+
+#KD - Pax
+exp_dirs <- Sys.glob(file.path(raw_data_base_dir,'100ug_KD/Pax_*/adhesion_props/models/'))
+exp_dirs <- exp_dirs[file_test('-d',exp_dirs)]
+raw_data$KD_Pax$intensity = load_results(exp_dirs,file.path('Average_adhesion_signal.Rdata'));
+
+#IA32 - Pax
+exp_dirs <- Sys.glob(file.path(raw_data_base_dir,'100ug_IA32/Pax_*/adhesion_props/models/'))
+exp_dirs <- exp_dirs[file_test('-d',exp_dirs)]
+raw_data$WT_Pax$intensity = load_results(exp_dirs,file.path('Average_adhesion_signal.Rdata'));
+
+#KD - FAK 
+exp_dirs <- Sys.glob(file.path(raw_data_base_dir,'100ug_KD/FAK_*/adhesion_props/models/'))
+exp_dirs <- exp_dirs[file_test('-d',exp_dirs)]
+raw_data$KD_FAK$intensity = load_results(exp_dirs,file.path('Average_adhesion_signal.Rdata'));
+
+#IA32 - FAK 
+exp_dirs <- Sys.glob(file.path(raw_data_base_dir,'100ug_IA32/FAK_*/adhesion_props/models/'))
+exp_dirs <- exp_dirs[file_test('-d',exp_dirs)]
+raw_data$WT_FAK$intensity = load_results(exp_dirs,file.path('Average_adhesion_signal.Rdata'));
+
+#KD - Vin
+exp_dirs <- Sys.glob(file.path(raw_data_base_dir,'100ug_KD/Vin_*/adhesion_props/models/'))
+exp_dirs <- exp_dirs[file_test('-d',exp_dirs)]
+raw_data$KD_Vin$intensity = load_results(exp_dirs,file.path('Average_adhesion_signal.Rdata'));
+
+#IA32 - Vin
+exp_dirs <- Sys.glob(file.path(raw_data_base_dir,'100ug_IA32/Vin_*/adhesion_props/models/'))
+exp_dirs <- exp_dirs[file_test('-d',exp_dirs)]
+raw_data$WT_Vin$intensity = load_results(exp_dirs,file.path('Average_adhesion_signal.Rdata'));
+
+########################################
+# Fibronectin Percentages
+########################################
+
+#Fibro 1ug - Pax
+exp_dirs <- Sys.glob(file.path(raw_data_base_dir,'Fibro_1ug_trial_2/*/adhesion_props/models/'))
+exp_dirs <- exp_dirs[file_test('-d',exp_dirs)]
+raw_data$Fibro_1ug$intensity = load_results(exp_dirs,file.path('Average_adhesion_signal.Rdata'));
+
+#Fibro 10ug - Pax
+exp_dirs <- Sys.glob(file.path(raw_data_base_dir,'Fibro_10ug_trial_2/*/adhesion_props/models/'))
+exp_dirs <- exp_dirs[file_test('-d',exp_dirs)]
+raw_data$Fibro_10ug$intensity = load_results(exp_dirs,file.path('Average_adhesion_signal.Rdata'));
+
+#Fibro 100ug - Pax
+exp_dirs <- Sys.glob(file.path(raw_data_base_dir,'Fibro_100ug_trial_2/*/adhesion_props/models/'))
+exp_dirs <- exp_dirs[file_test('-d',exp_dirs)]
+raw_data$Fibro_100ug$intensity = load_results(exp_dirs,file.path('Average_adhesion_signal.Rdata'));
+
+print('Done Loading Data')
+
+###########################################################
+#Result filtering
+###########################################################
+total_models = 0;
+for (exp_type in names(raw_data)) {
+    total_models = total_models + find_number_of_models(raw_data[[exp_type]]$intensity);
+}
+
+processed = list();
+dynamic_props = list();
+full_cell_props = list();
+static_props = list();
+for (exp_type in names(raw_data)) {
+    for (property in names(raw_data[[exp_type]])) {
+        if (property != "intensity") {
+            next;
+        }
+        print(paste("Filtering", exp_type, property));
+        
+        processed$no_filt[[exp_type]][[property]] = filter_results(raw_data[[exp_type]][[property]], 
+            min.r.sq = -Inf, max.p.val = Inf, pos.slope=FALSE);
+        
+        processed$only_signif[[exp_type]][[property]] = filter_results(raw_data[[exp_type]][[property]], 
+            min.r.sq = -Inf, max.p.val = 0.05, model_count = total_models);
+
+    }
+    
+    full_cell_props[[exp_type]] = gather_global_exp_summary(raw_data[[exp_type]]$intensity)
+    dynamic_props[[exp_type]] = gather_general_dynamic_props(raw_data[[exp_type]]$intensity)
+}
+
+print('Done Filtering Data')
+out_folder = '../../doc/publication/figures/emma'
+dir.create(out_folder,recursive=TRUE, showWarnings=FALSE);
+
+KD_Pax = processed$only_signif$KD_Pax$intensity;
+KD_Pax_nf = processed$no_filt$KD_Pax$intensity;
+IA32_Pax = processed$only_signif$WT_Pax$intensity;
+IA32_Pax_nf = processed$no_filt$WT_Pax$intensity;
+
+KD_Vin = processed$only_signif$KD_Vin$intensity;
+IA32_Vin = processed$only_signif$WT_Vin$intensity;
+
+KD_FAK = processed$only_signif$KD_FAK$intensity;
+IA32_FAK = processed$only_signif$WT_FAK$intensity;
+
+Fibro_1ug = processed$only_signif$Fibro_1ug$intensity;
+Fibro_10ug = processed$only_signif$Fibro_10ug$intensity;
+Fibro_100ug = processed$only_signif$Fibro_100ug$intensity;
+
+# rate_angle_data = list()
+# rate_angle_data$Fibro_100ug = get_angle_data_set(raw_data$Fibro_100ug$intensity)
+# rate_angle_data$Fibro_10ug = get_angle_data_set(raw_data$Fibro_10ug$intensity)
+# rate_angle_data$Fibro_1ug = get_angle_data_set(raw_data$Fibro_1ug$intensity)
+# 
+# split_data = list()
+# split_data$Fibro_100ug = split_angle_data(rate_angle_data$Fibro_100ug)
+# split_data$Fibro_10ug = split_angle_data(rate_angle_data$Fibro_10ug)
+# split_data$Fibro_1ug = split_angle_data(rate_angle_data$Fibro_1ug)
+ 
+stop()
+ 
+###########################################################
+# P-value calculations
+###########################################################
+
+Pax_p = list();
+Pax_p$assembly = determine_median_p_value(IA32_Pax$assembly$slope,KD_Pax$assembly$slope)
+Pax_p$disassembly = determine_median_p_value(IA32_Pax$disassembly$slope,KD_Pax$disassembly$slope)
+
+FAK_p = list();
+FAK_p$assembly = determine_median_p_value(IA32_FAK$assembly$slope,KD_FAK$assembly$slope)
+FAK_p$disassembly = determine_median_p_value(IA32_FAK$disassembly$slope,KD_FAK$disassembly$slope)
+
+Vin_p = list();
+Vin_p$assembly = determine_median_p_value(IA32_Vin$assembly$slope,KD_Vin$assembly$slope)
+Vin_p$disassembly = determine_median_p_value(IA32_Vin$disassembly$slope,KD_Vin$disassembly$slope)
+
+fibro_1ug_10ug_p = list()
+fibro_1ug_10ug_p$assembly = determine_median_p_value(Fibro_1ug$assembly$slope,Fibro_10ug$assembly$slope)
+fibro_1ug_10ug_p$disassembly = determine_median_p_value(Fibro_1ug$disassembly$slope,Fibro_10ug$disassembly$slope)
+
+fibro_1ug_100ug_p = list()
+fibro_1ug_100ug_p$assembly = determine_median_p_value(Fibro_1ug$assembly$slope,Fibro_100ug$assembly$slope)
+fibro_1ug_100ug_p$disassembly = determine_median_p_value(Fibro_1ug$disassembly$slope,Fibro_100ug$disassembly$slope)
+
+fibro_10ug_100ug_p = list()
+fibro_10ug_100ug_p$assembly = determine_median_p_value(Fibro_10ug$assembly$slope,Fibro_100ug$assembly$slope)
+fibro_10ug_100ug_p$disassembly = determine_median_p_value(Fibro_10ug$disassembly$slope,Fibro_100ug$disassembly$slope)
+
+prep_time = proc.time()
+system("notify-send \"done reading in R data\"");
+stop()
+
+################################################################################
+#Plotting
+################################################################################
+
+########################################
+# All-by-All Plots
+#######################################
+
+# library(car)
+# data_of_interest = c('slope','length','largest_area','ad_sig','mean_axial_ratio','adj.r.squared')
+# 
+# data_and_file = list('IA32_Pax_assembly.pdf'=IA32_Pax$assembly, 'IA32_Pax_disassembly.pdf'=IA32_Pax$dis,
+#     'IA32_Vin_assembly.pdf'=IA32_Vin$assembly, 'IA32_Vin_disassembly.pdf'=IA32_Vin$dis,
+#     'KD_Pax_assembly.pdf'=KD_Pax$assembly, 'KD_Pax_disassembly.pdf'=KD_Pax$dis,
+#     'KD_Vin_assembly.pdf'=KD_Vin$assembly, 'KD_Vin_disassembly.pdf'=KD_Vin$dis
+# )
+# 
+# for (file_name in names(data_and_file)) {
+#     pdf(file.path(out_folder,file_name))
+#     lm_sum = summary(lm(slope ~ length + largest_area + ad_sig + mean_axial_ratio + adj.r.squared, 
+#         data=data_and_file[[file_name]]))
+#     scatterplot.matrix(subset(data_and_file[[file_name]],select = data_of_interest),pch=19,
+#         main=paste(file_name,' R^2=',sprintf('%0.3f',lm_sum$adj.r.squared),sep=''))
+#     graphics.off()
+# }
+# 
+# for (prop in data_of_interest) {
+#     pdf(file.path(out_folder,paste('boxplots_',prop,'.pdf',sep='')))
+#     boxplot(IA32_Pax$assem[[prop]],IA32_Vin$assem[[prop]],KD_Pax$assem[[prop]],KD_Vin$assem[[prop]],
+#         names=c('IA32 Pax','IA32 Vin','KD Pax','KD Vin'),
+#         notch=T,main=paste('Assembly',prop))
+#     boxplot(IA32_Pax$dis[[prop]],IA32_Vin$dis[[prop]],KD_Pax$dis[[prop]],KD_Vin$dis[[prop]],
+#         names=c('IA32 Pax','IA32 Vin','KD Pax','KD Vin'),
+#         notch=T,main=paste('Disassembly',prop))
+#     graphics.off()
+# }
+
+########################################
+#Single Ad Plots
+#######################################
+
+# plot_all_ad_intensities(raw_data$IA32_Pax$intensity,IA32_Pax)
+# plot_all_ad_intensities(raw_data$IA32_Vin$intensity,IA32_Vin)
+# plot_all_ad_intensities(raw_data$KD_Pax$intensity,KD_Pax)
+# plot_all_ad_intensities(raw_data$KD_Vin$intensity,KD_Vin)
+ 
+###########################################################
+#Kinetics Figures
+###########################################################
+
+#######################################
+# Fibronectin Concentrations
+#######################################
+
+svg(file.path(out_folder,'Kinetics','fibro_concen_kinetics.svg'),width=8,height=5);
+layout(cbind(1,2))
+par(bty='n',mar=c(2.8,2.8,3,0), mgp=c(1.6,0.5,0),xpd=T)
+
+#assembly plot
+boxplot_with_points(list(Fibro_1ug$assembly$slope,Fibro_10ug$assembly$slope,Fibro_100ug$assembly$slope),
+    names=c('1','10','100'),with.p.value=F,inc.points=F,
+    ylab=expression(paste('FA Assembly Rate (',min^-1,')',sep='')))
+
+pl_size = par("usr");
+
+mtext('Fibronectin Concentration (\u03BCg/mL)',side=1,line=1.7)
+
+plot_signif_bracket(c(1,pl_size[4]*0.93),c(2,pl_size[4]*0.92), 
+    over_text=paste('p<', fibro_1ug_10ug_p$assembly$p.value[1],sep=''),text_sep=0)
+plot_signif_bracket(c(2,pl_size[4]*0.99),c(3,pl_size[4]*0.98), 
+    over_text=paste('p<',fibro_10ug_100ug_p$assembly$p.value[1],sep=''))
+plot_signif_bracket(c(1,pl_size[4]*1.08),c(3,pl_size[4]*1.07), 
+    over_text=paste('p<',fibro_1ug_100ug_p$assembly$p.value[1],sep=''))
+
+#disassembly plot
+boxplot_with_points(list(Fibro_1ug$dis$slope,Fibro_10ug$dis$slope,Fibro_100ug$dis$slope),
+    names=c('1','10','100'),with.p.value=F,inc.points=F,
+    ylab=expression(paste('FA Disassembly Rate (',min^-1,')',sep='')))
+
+pl_size = par("usr");
+mtext('Fibronectin Concentration (\u03BCg/mL)',side=1,line=1.7)
+plot_signif_bracket(c(1,pl_size[4]*0.93),c(2,pl_size[4]*0.92), 
+    over_text=paste('p<', fibro_1ug_10ug_p$dis$p.value[1],sep=''),text_sep=0)
+plot_signif_bracket(c(2,pl_size[4]*0.99),c(3,pl_size[4]*0.98), 
+    over_text=paste('p<',fibro_10ug_100ug_p$dis$p.value[1],sep=''))
+plot_signif_bracket(c(1,pl_size[4]*1.08),c(3,pl_size[4]*1.07), 
+    over_text=paste('p<',fibro_1ug_100ug_p$dis$p.value[1],sep=''))
+
+graphics.off()
+
+#######################################
+# Different Adhesion Tags
+#######################################
+
+svg(file.path(out_folder,'Kinetics','all_tagged_kinetics.svg'),width=8,height=10);
+layout(rbind(1,2))
+par(bty='n',mar=c(2.8,2.8,2,0), mgp=c(1.6,0.5,0),xpd=T)
+
+#assembly plot
+boxplot_with_points(list(IA32_Pax$assembly$slope,KD_Pax$assembly$slope,
+    IA32_FAK$assembly$slope,KD_FAK$assembly$slope,
+    IA32_Vin$assembly$slope, KD_Vin$assembly$slope),
+    with.p.value=F,inc.points=F, axes=F,
+    ylab=expression(paste('FA Assembly Rate (',min^-1,')',sep='')))
+
+pl_size = par("usr");
+char_size = par("cxy")[2]
+axis(2)
+
+labs = c(paste('NS (n=',length(IA32_Pax$assembly$slope),')',sep=''),
+    paste('2xKD (n=',length(KD_Pax$assembly$slope),')',sep=''))
+axis(1, labels = labs, at = c(1,2))
+lines(c(1,2),rep(pl_size[3]-char_size*1.675,2),lwd=3)
+mtext('Pax',1,at=1.5,line=1.7)
+plot_signif_bracket(c(1,pl_size[4]*1.01),c(2,pl_size[4]*1), 
+    over_text=paste('p<', Pax_p$assembly$p.value[1],sep=''),text_sep=0)
+
+labs = c(paste('NS (n=',length(IA32_FAK$assembly$slope),')',sep=''),
+    paste('2xKD (n=',length(KD_FAK$assembly$slope),')',sep=''))
+axis(1, labels = labs, at = c(3,4))
+lines(c(3,4),rep(pl_size[3]-char_size*1.675,2),lwd=3)
+mtext('FAK',1,at=3.5,line=1.7)
+plot_signif_bracket(c(3,pl_size[4]*1.01),c(4,pl_size[4]*1), 
+    over_text=paste('p<', FAK_p$assembly$p.value[1],sep=''),text_sep=0)
+
+labs = c(paste('NS (n=',length(IA32_Vin$assembly$slope),')',sep=''),
+    paste('2xKD (n=',length(KD_Vin$assembly$slope),')',sep=''))
+axis(1, labels = labs, at = c(5,6))
+lines(c(5,6),rep(pl_size[3]-char_size*1.675,2),lwd=3)
+mtext('Vin',1,at=5.5,line=1.7)
+plot_signif_bracket(c(5,pl_size[4]*1.01),c(6,pl_size[4]*1), 
+    over_text=paste('p<', Vin_p$assembly$p.value[1],sep=''),text_sep=0)
+
+#disassembly plot
+boxplot_with_points(list(IA32_Pax$dis$slope,KD_Pax$dis$slope,
+    IA32_FAK$dis$slope,KD_FAK$dis$slope,
+    IA32_Vin$dis$slope, KD_Vin$dis$slope),
+    with.p.value=F,inc.points=F, axes=F,
+    ylab=expression(paste('FA Disassembly Rate (',min^-1,')',sep='')))
+
+pl_size = par("usr");
+char_size = par("cxy")[2]
+axis(2)
+
+labs = c(paste('NS (n=',length(IA32_Pax$dis$slope),')',sep=''),
+    paste('2xKD (n=',length(KD_Pax$dis$slope),')',sep=''))
+axis(1, labels = labs, at = c(1,2))
+lines(c(1,2),rep(pl_size[3]-char_size*1.675,2),lwd=3)
+mtext('Pax',1,at=1.5,line=1.7)
+plot_signif_bracket(c(1,pl_size[4]*1.01),c(2,pl_size[4]*1), 
+    over_text=paste('p<', Pax_p$dis$p.value[1],sep=''),text_sep=0)
+
+labs = c(paste('NS (n=',length(IA32_FAK$dis$slope),')',sep=''),
+    paste('2xKD (n=',length(KD_FAK$dis$slope),')',sep=''))
+axis(1, labels = labs, at = c(3,4))
+lines(c(3,4),rep(pl_size[3]-char_size*1.675,2),lwd=3)
+mtext('FAK',1,at=3.5,line=1.7)
+plot_signif_bracket(c(3,pl_size[4]*1.01),c(4,pl_size[4]*1), 
+    over_text=paste('p<', FAK_p$dis$p.value[1],sep=''),text_sep=0)
+
+labs = c(paste('NS (n=',length(IA32_Vin$dis$slope),')',sep=''),
+    paste('2xKD (n=',length(KD_Vin$dis$slope),')',sep=''))
+axis(1, labels = labs, at = c(5,6))
+lines(c(5,6),rep(pl_size[3]-char_size*1.675,2),lwd=3)
+mtext('Vin',1,at=5.5,line=1.7)
+plot_signif_bracket(c(5,pl_size[4]*1.01),c(6,pl_size[4]*1), 
+    over_text=paste('p<', Vin_p$dis$p.value[1],sep=''),text_sep=0)
+
+graphics.off()
+
+###########################################################
+# FA Alignment Comparisons
+###########################################################
+
+source('FA_alignment_search.R')
+svg(file.path(out_folder,'Kinetics','angle_split.svg'),width=7,height=3);
+layout(cbind(1,2,3))
+par(bty='n',mar=c(1.8,2.8,0,0), mgp=c(1.6,0.5,0),xpd=T)
+boxplot_with_points(list(split_data$Fibro_100ug$middle_assembly$assembly_rate,
+    split_data$Fibro_100ug$out_assembly$assembly_rate),notch=T,
+    names=c('Middle','Out'),with.p.value=F, inc.points=F,
+    p.value.type = 'median',ylab=expression(paste('FA Assembly Rate (',min^-1,')',sep='')))
+
+boxplot_with_points(list(split_data$Fibro_100ug$middle_disassembly$disassembly_rate,
+    split_data$Fibro_100ug$out_disassembly$disassembly_rate),notch=T,
+    names=c('Middle','Out'),with.p.value=F,inc.points=F,
+    p.value.type = 'median',ylab=expression(paste('FA Disassembly Rate (',min^-1,')',sep='')))
+
+boxplot_with_points(list(split_data$Fibro_100ug$middle_longev$longevity*2.5,
+    split_data$Fibro_100ug$out_longev$longevity*2.5),notch=T,
+    names=c('Middle','Out'),with.p.value=F,inc.points = F,
+    ylab='Longevity (min)')
+graphics.off()
