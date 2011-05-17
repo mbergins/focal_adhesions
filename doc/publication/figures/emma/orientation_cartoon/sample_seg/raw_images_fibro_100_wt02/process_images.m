e090c709d0771e533ed54b05d36c3862eee741f1
mbergins <mbergins@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2011-05-17 21:44:45 +0000

    emma figures: 	-updating FA orientation description 	-adding copies of the simple orientation figures
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/focal_adhesions/trunk@2847 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- doc/publication/figures/emma/orientation_cartoon/sample_seg/raw_images_fibro_100_wt02/process_images.m
+++ doc/publication/figures/emma/orientation_cartoon/sample_seg/raw_images_fibro_100_wt02/process_images.m
@@ -2,25 +2,26 @@
 %% File Reading
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% addpath('/home/mbergins/Documents/Projects/focal_adhesions/trunk/src/visualize_cell_features/')
-% 
-% adhesions = imread('adhesions_perim.png');
-% focal_image = double(imread('focal_image.png'));
-% focal_norm = (focal_image - min(focal_image(:)))/range(focal_image(:));
-% 
-% col_1 = 145;
-% col_2 = 382;
-% row_1 = 500;
-% row_2 = 699;
+addpath('/home/mbergins/Documents/Projects/focal_adhesions/trunk/src/visualize_cell_features/')
+
+adhesions = imread('adhesions_perim.png');
+adhesions_full = imread('adhesions.png');
+focal_image = double(imread('focal_image.png'));
+focal_norm = (focal_image - min(focal_image(:)))/range(focal_image(:));
+
+col_1 = 145;
+col_2 = 382;
+row_1 = 500;
+row_2 = 699;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Highlight Creation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% imwrite(focal_norm(row_1:row_2,col_1:col_2),'focal_norm.png')
-% 
-% ad_highlights = create_highlighted_image(focal_norm,adhesions > 0,'color_map',[0,0/255,255/255]);
-% imwrite(ad_highlights(row_1:row_2,col_1:col_2,:),'ad_highlights.png')
+imwrite(focal_norm(row_1:row_2,col_1:col_2),'focal_norm.png')
+
+ad_highlights = create_highlighted_image(focal_norm,adhesions > 0,'color_map',[105/255,249/255,255/255]);
+imwrite(ad_highlights(row_1:row_2,col_1:col_2,:),'ad_highlights.png')
 % 
 % major_axis = csvread('raw_data/MajorAxisLength.csv');
 % minor_axis = csvread('raw_data/MinorAxisLength.csv');
@@ -59,18 +60,17 @@
 %         pos_str, label_str, ' ', output_file];
 %     system(command_str);
 % end
-
-%Pull out adhesion 172
-
-ad_172 = adhesions == 172;
-ad_172_high = create_highlighted_image(focal_norm,ad_172,'color_map',[0,1,0],'mix_percent',0.5);
-limits = [543,563,330,340];
-imwrite(ad_172_high(limits(1):limits(2),limits(3):limits(4),:),'ad_172_high.png')
-
-%Pull out adhesion 100
-ad_100 = adhesions == 100;
-ad_100_high = create_highlighted_image(focal_norm,ad_100,'color_map',[0,1,0],'mix_percent',0.5);
-limits = [650,672,276,287];
-
-imwrite(ad_100_high(limits(1):limits(2),limits(3):limits(4),:),'ad_100_high.png')
-
+% 
+% Pull out adhesion 172
+% 
+% ad_172 = adhesions == 172;
+% ad_172_high = create_highlighted_image(focal_norm,ad_172,'color_map',[0,1,0],'mix_percent',0.5);
+% limits = [543,563,330,340];
+% imwrite(ad_172_high(limits(1):limits(2),limits(3):limits(4),:),'ad_172_high.png')
+% 
+% %Pull out adhesion 100
+% ad_100 = adhesions == 100;
+% ad_100_high = create_highlighted_image(focal_norm,ad_100,'color_map',[0,1,0],'mix_percent',0.5);
+% limits = [650,672,276,287];
+% 
+% imwrite(ad_100_high(limits(1):limits(2),limits(3):limits(4),:),'ad_100_high.png')
