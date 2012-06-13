5a3585d483b322a647237b57a15e8df4902da811
mbergins@ISIS.UNC.EDU <mbergins@ISIS.UNC.EDU@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2012-06-13 17:45:35 +0000

    register_with_nifty.m: 	-adding code to remove regions outside registered
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/focal_adhesions/trunk@7407 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/find_cell_features/register_with_nifty.m
+++ src/find_cell_features/register_with_nifty.m
@@ -33,6 +33,7 @@ image_dirs = image_dirs(3:end);
 %registration will go in a step-wise fashion, register image 2 to image 1,
 %then image 3 to 2, then image 4 to 3, ...
 for i_num = 1:(size(image_dirs,1)-1)
+% for i_num = 1:
     FA_file1 = fullfile(base_dir,image_dirs(i_num).name,filenames.focal_image);
     FA_file2 = fullfile(base_dir,image_dirs(i_num+1).name,filenames.focal_image);
 
@@ -49,17 +50,52 @@ for i_num = 1:(size(image_dirs,1)-1)
     save_nii(img2_nii,file_nii_temp_2);
     
     reg_result_file = fullfile(base_dir,image_dirs(i_num+1).name,'reg_result.nii');
+    affine_file = fullfile(base_dir,image_dirs(i_num+1).name,'reg_values.txt');
     
     system(['reg_aladin -source ', file_nii_temp_2, ' -target ',file_nii_temp_1, ...
-        ' -result ',reg_result_file, ' -aff /dev/null -noRot -rigOnly >/dev/null 2>/dev/null']);
-    
+        ' -result ',reg_result_file, ' -aff ',affine_file,' -noRot -rigOnly >/dev/null 2>/dev/null']);
     reg_result = load_nii(reg_result_file);
     
     imwrite(reg_result.img,FA_file2);
     
     delete(file_nii_temp_1,file_nii_temp_2,reg_result_file);
-    1;
+    
+    if (mod(i_num,round(size(image_dirs,1)/10)) == 0)
+        fprintf('Done with %d/%d\n',i_num,size(image_dirs,1));
+    end
 end
+
+%The output images are the same size as the input images, with the regions
+%outside the image after registration filled in with zeros or near zeros.
+%We want to determine where these filled in regions are and remove them
+%from all the images to avoid edge effects. First we build a mask that only
+%contains the image regions.
+image_mask = [];
+for i_num = 2:size(image_dirs,1)
+    FA_file = fullfile(base_dir,image_dirs(i_num).name,filenames.focal_image);
+
+    FA_img = imread(FA_file);
+    FA_img_binary = FA_img >= 5;
+    if (size(image_mask,1) == 0)
+        image_mask = FA_img_binary;
+    else
+        image_mask = FA_img_binary & image_mask;
+    end
+end
+lims = [find(sum(image_mask,2),1,'first')+1,find(sum(image_mask,2),1,'last')-1;
+    find(sum(image_mask),1,'first')+1,find(sum(image_mask),1,'last')-1];
+
+%Now we apply the mask to the images in the set
+for i_num = 1:size(image_dirs,1)
+    FA_file = fullfile(base_dir,image_dirs(i_num).name,filenames.focal_image);
+
+    FA_img = imread(FA_file);
+    
+    FA_img = FA_img(lims(1,1):lims(1,2),lims(2,1):lims(2,2));
+    
+    imwrite(FA_img,FA_file);
+end
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Functions
