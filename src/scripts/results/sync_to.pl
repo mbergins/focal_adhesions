e1cc1149977f4e8cee99ebb56db4e7c9b2fea033
mbergins@ISIS.UNC.EDU <mbergins@ISIS.UNC.EDU@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2012-07-13 14:45:23 +0000

    sync_to.pl: 	-porting over improvements from the invadopodia code base
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/focal_adhesions/trunk@7521 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/scripts/results/sync_to.pl
+++ src/scripts/results/sync_to.pl
@@ -8,7 +8,9 @@ $opt{server} = "NOSERVER";
 $opt{no_time} = 0;
 $opt{repeat} = 1;
 $opt{delay} = 0;
-GetOptions(\%opt,"server=s", "debug|d", "progress", "no_time|nt", "repeat=s", "delay=s") or die;
+$opt{exclude} = "";
+GetOptions(\%opt,"server=s", "debug|d", "progress", "no_time|nt", "repeat=s", 
+	"delay=s", "exclude=s") or die;
 
 my $progress_str = "";
 if ($opt{progress}) {
@@ -21,20 +23,29 @@ if ($opt{no_time}) {
 }
 
 my $rsync_command = "rsync";
-if (-e "/nas02/home/m/b/mbergins/bin/rsync") { 
+if (-e "/nas02/home/m/b/mbergins/bin/rsync") {
 	$rsync_command = "/nas02/home/m/b/mbergins/bin/rsync";
 }
 
-my $command = "$time_str" . "$rsync_command $progress_str" . "-a --exclude data.stor ../../results/* $opt{server}:~/Documents/Projects/focal_adhesions/trunk/results/";
+my $exclude_str = '';
+if ($opt{exclude}) {
+	for my $i (split(",",$opt{exclude})) {
+		$exclude_str .= " --exclude=**$i**";
+	}
+}
+
+my $command = "$time_str $rsync_command $progress_str $exclude_str " . 
+	"--exclude=**data.stor** -a ../../results/* " .
+	"$opt{server}:~/Documents/Projects/focal_adhesions/trunk/results/";
 if ($opt{server} eq "NOSERVER" || $opt{debug}) {
     print "$command\n";
 } else {
     while($opt{repeat} != 0) {
-        my $return = system "$command";
-        print "Done with sync $opt{repeat} Return code: $return\n\n";
-		if ($return == 65280) {
+		my $return = system "$command";
+	  	print "Done with sync $opt{repeat} Return code: $return\n\n";
+	    if ($return == 65280) { 
 			print "Caught exit code.";
-			last;
+			last; 
 		}
         $opt{repeat}--;
         sleep $opt{delay}*60;
