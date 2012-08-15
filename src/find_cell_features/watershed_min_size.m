85f41c5f26e0e04033483027a3578167e8e45828
mbergins@ISIS.UNC.EDU <mbergins@ISIS.UNC.EDU@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2012-08-15 17:59:04 +0000

    watershed_min_size.m: 	-major overhaul of watershed seg method, removed most of the bottlenecks, 	running five times faster
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/focal_adhesions/trunk@7583 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/find_cell_features/watershed_min_size.m
+++ src/find_cell_features/watershed_min_size.m
@@ -38,132 +38,116 @@ for this_ad_num = 1:max(label_mat(:))
         this_ad_image(sorted_linear_indexes(ad_int_ind)) = -Inf;
     end
     
+    %Up till the minimum independent size barrier, we can't have any
+    %watershed splitting events, so go ahead and seed in up to the minimum
+    %size pixels and label them using bwlabel
     watershed_ad = zeros(size(label_mat,1),size(label_mat,2));
+    for i=1:min_size
+       watershed_ad(sorted_linear_indexes(1)) = 1;
+       sorted_linear_indexes = sorted_linear_indexes(2:end);
+    end
+    
+    watershed_ad = bwlabel(watershed_ad,4);
+    
     while (length(sorted_linear_indexes) >= 1)
-        watershed_ad = add_single_pixel(watershed_ad,sorted_linear_indexes(1),min_size);
+        watershed_ad = add_single_pixel_watershed(watershed_ad,sorted_linear_indexes(1),min_size);
         sorted_linear_indexes = sorted_linear_indexes(2:end);
     end
     
     %add the watershed segmented FA back to the master segmentation
     unique_labels = unique(watershed_ad(:));
-    assert(unique_labels(1) == 0)
     for j=2:length(unique_labels);
         final_label_mat(watershed_ad == unique_labels(j)) = max(final_label_mat(:)) + 1;
     end
 end
 
-function ad_zamir = add_single_pixel(ad_zamir,pix_pos,min_size)
 
-[pix_pos_ind(1),pix_pos_ind(2)] = ind2sub(size(ad_zamir),pix_pos);
+function watershed_ads = add_single_pixel_watershed(watershed_ads,pix_pos,min_size)
 
-%save the number of pixels in currently in adhesions, this will be compared
-%at the end of processing to make sure another pixel has been added
-initial_size = sum(sum(im2bw(ad_zamir,0)));
+initial_size = sum(sum(watershed_ads > 0));
 
-%now locate the adhesions in the current adhesions that touch the newest
-%selected pixel, store these adhesions in connected_ad
-connected_ad = false(size(ad_zamir));
-connected_ad(pix_pos) = 1;
-ad_nums = zeros(4);
+[pix_pos_ind(1),pix_pos_ind(2)] = ind2sub(size(watershed_ads),pix_pos);
+
+touching_ad_nums = zeros(4,1);
 %wrap these calls in try, since they could attempt to access indexes
 %outside the 1-size range
-try ad_nums(1) = ad_zamir(pix_pos_ind(1) - 1,pix_pos_ind(2)); end %#ok<TRYNC>
-try ad_nums(2) = ad_zamir(pix_pos_ind(1) + 1,pix_pos_ind(2)); end %#ok<TRYNC>
-try ad_nums(3) = ad_zamir(pix_pos_ind(1),pix_pos_ind(2) - 1); end %#ok<TRYNC>
-try ad_nums(4) = ad_zamir(pix_pos_ind(1),pix_pos_ind(2) + 1); end %#ok<TRYNC>
+try touching_ad_nums(1) = watershed_ads(pix_pos_ind(1) - 1,pix_pos_ind(2)); end %#ok<TRYNC>
+try touching_ad_nums(2) = watershed_ads(pix_pos_ind(1) + 1,pix_pos_ind(2)); end %#ok<TRYNC>
+try touching_ad_nums(3) = watershed_ads(pix_pos_ind(1),pix_pos_ind(2) - 1); end %#ok<TRYNC>
+try touching_ad_nums(4) = watershed_ads(pix_pos_ind(1),pix_pos_ind(2) + 1); end %#ok<TRYNC>
 
-for i = 1:length(ad_nums)
-    if (ad_nums(i) ~= 0)
-        connected_ad(ad_zamir == ad_nums(i)) = 1;
-    end
-end
-
-%build a binary image of the current touching adhesions, without the new
-%pixel
-old_ad = false(size(ad_zamir));
-old_ad(and(ad_zamir > 0,connected_ad)) = 1;
-
+touching_ad_nums = unique(touching_ad_nums);
+touching_ad_nums = touching_ad_nums(touching_ad_nums ~= 0);
 %if there aren't any pixels which were connected to newest pixel, add the
 %newest pixel as a new adhesion, otherwise, start a more complicated
 %procedure
-if (sum(old_ad(:)) == 0)
-    ad_zamir(connected_ad) = max(ad_zamir(:)) + 1;
+if (sum(touching_ad_nums) == 0)
+    watershed_ads(pix_pos) = max(watershed_ads(:)) + 1;
 else
-    %build an image with the connected old adhesions renumbered to start
-    %with one, since it will be easier to work with the regionprops output when
-    %numbered from one
-    relabeled_old_ad = ad_zamir;
-    relabeled_old_ad(old_ad ~= 1) = 0;
-    ad_nums = unique(relabeled_old_ad);
-    assert(ad_nums(1) == 0, 'Error in collecting relabeled_old_ad unique ad numbers')
-    for i = 2:length(ad_nums)
-        relabeled_old_ad(relabeled_old_ad == ad_nums(i)) = i - 1;
-    end
-    assert(all(unique(relabeled_old_ad)' == 0:(length(ad_nums) - 1)), 'Error in old ad relabeling')
-    assert(sum(connected_ad(:)) == (sum(old_ad(:)) + 1),'Error in connected ad finding: %d, %d ',sum(connected_ad(:)),sum(old_ad(:)))
-    
-    props = regionprops(relabeled_old_ad,'Area','Centroid');
-
-    %if there is only one set of props, we know there was only one adhesion
-    %connected to the newest pixel, otherwise, trigger a more complicated
-    %procedure
-    if (length(props) == 1)
-        %pick out the adhesion number from the first entry in find, then
-        %check that all other pixels have the same adhesion number and
-        %assign the newest pixel to the old adhesion
-        ad_number = ad_zamir(find(relabeled_old_ad == 1,1));
-        assert(ad_number > 0, 'Error in old ad filtering: adhesion number less than 1');
-        assert(all(ad_number == ad_zamir(relabeled_old_ad >= 1)),'Error in old ad filtering: single adhesion with different numbers');
-
-        ad_zamir(pix_pos) = ad_number;
+    %if there is only one touching adhesion, label the new pixel with that
+    %adhesion's label
+    if (length(touching_ad_nums) == 1)
+        watershed_ads(pix_pos) = touching_ad_nums(1);
     else
+        touching_ads = ismember(watershed_ads,touching_ad_nums).*watershed_ads;
+        
+        props = regionprops(touching_ads,'Area','Centroid');
+        
         %Check if all the connected adhesions are below the minimum size,
         %if they are, merge all the adhesions, otherwise, trigger a more
         %complicated procedure
-        meets_min = [props.Area] >= min_size;
+        meets_size_min = [props.Area] >= min_size;
+        
         if (all([props.Area] < min_size))
-            ad_zamir(connected_ad) = min(ad_zamir(old_ad == 1));
-        elseif (sum(meets_min) == 1)
-            %There is only one large adhesion touching, find the current
-            %adhesion number and add the new pixel
-            large_area_ad = ismember(relabeled_old_ad,find([props.Area] >= min_size));
-
-            ad_number = ad_zamir(find(large_area_ad == 1,1));
-            assert(ad_number > 0, 'Error in large ad filtering: adhesion number less than 1');
-            assert(all(ad_number == ad_zamir(large_area_ad == 1)),'Error in large ad filtering: single adhesion with different numbers');
-
-            ad_zamir(connected_ad) = ad_number;
-        else
-            %There are two or more large adhesions touching the new pixel,
-            %figure out which adhesion is closer in terms of centroid to
-            %the new pixel and add the new pixel to that adhesion, if there
-            %is a tie, just use the first adhesion in the list
-            large_area_ads = ismember(relabeled_old_ad,find([props.Area] >= min_size));
-            large_area_nums = unique(relabeled_old_ad(large_area_ads));
-
-            %Also, the new pixel might be connected to small adhesions as
-            %well as large adhesions, so capture the position of the small
-            %adhesions for later assignment to selected large adhesion
-            small_area_ads = ismember(relabeled_old_ad, find([props.Area] < min_size));
-
-            closest_relabeled_info = [0,inf];
-            for i = 1:length(large_area_nums)
-                dist = sqrt((pix_pos_ind(1) - props(i).Centroid(2))^2 + (pix_pos_ind(2) - props(i).Centroid(1))^2);
-                if (dist < closest_relabeled_info(2))
-                    closest_relabeled_info = [i,dist];
+            min_ad_num = min(touching_ad_nums);
+            watershed_ads(touching_ads > 0) = min_ad_num;
+            watershed_ads(pix_pos) = min_ad_num;
+            1;
+        elseif (sum(meets_size_min) == 1)
+            %There is only one large adhesion touching, find the adhesion
+            %number and label the new pixel with that number
+            large_ad_num = find(meets_size_min);
+            watershed_ads(pix_pos) = large_ad_num;
+            
+            doesnt_meet_size_min = find([props.Area] > 0 & [props.Area] < min_size); %#ok<EFIND>
+            if (not(isempty(doesnt_meet_size_min)))
+                %now assign those touching positions to the same adhesion
+                %as selected by the distance metric
+                for i=doesnt_meet_size_min
+                    watershed_ads(watershed_ads == i) = large_ad_num;
                 end
             end
-            assert(closest_relabeled_info(1) ~= 0, 'Error in identifying the closest of the large adhesions')
-
-            ad_number = ad_zamir(find(relabeled_old_ad == closest_relabeled_info(1),1));
-            assert(ad_number > 0, 'Error in largest ad filtering: adhesion number less than 1');
-            assert(all(ad_number == ad_zamir(relabeled_old_ad == closest_relabeled_info(1))),'Error in largest ad filtering: single adhesion with different numbers');
-
-            ad_zamir(pix_pos) = ad_number;
-            ad_zamir(small_area_ads) = ad_number;
+        else
+            %we now have at least two large adhesions touching the current
+            %pixel, we will decide on the assignment based on the distance
+            %to the centroids of the current objects
+            centroid_pos = reshape([props.Centroid],2,[]);
+            dists = (centroid_pos(1,:) - pix_pos_ind(2)).^2 + (centroid_pos(2,:) - pix_pos_ind(1)).^2;
+            
+            %we need to block any small adhesion distances that might also
+            %be touching the current position, highly unlikely, but just to
+            %make sure...
+            dists = dists.*meets_size_min;
+            
+            min_dist_ad_num = find(dists == min(dists),1,'first');
+            watershed_ads(pix_pos) = min_dist_ad_num;
+            
+            %now do a search for any small adhesions that didn't meet the
+            %minimum size threshold, but are still touching the current
+            %position, again, highly unlikely, but just in case...
+            doesnt_meet_size_min = find([props.Area] > 0 & [props.Area] < min_size); %#ok<EFIND>
+            
+            if (not(isempty(doesnt_meet_size_min)))
+                %now assign those touching positions to the same adhesion
+                %as selected by the distance metric
+                for i=doesnt_meet_size_min
+                    watershed_ads(watershed_ads == i) = min_dist_ad_num;
+                end
+            end
         end
 
     end
 end
 
-assert(initial_size + 1 == sum(sum(im2bw(ad_zamir,0))), 'Error in adding single pixel: Adhesion set did not grow')
+final_size = sum(sum(watershed_ads > 0));
+assert(initial_size + 1 == final_size)
