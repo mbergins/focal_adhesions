6da0e91c8bf5ab99e1df1e0ae3b7555e285839eb
mbergins@ISIS.UNC.EDU <mbergins@ISIS.UNC.EDU@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2012-12-10 23:03:31 +0000

    build_thresholded_image_sets.m: 	-re-worked thresholding image sets to work with octave 	-changes to deal with very large images
    thresh_testing.pl:
    	-moved all html into template pages
    
    threshold_results.tmpl:
    	-default webpage for thresholding results
    
    intro_page.tmpl:
    	-not using templates for this page
    
    threshold_start.tmpl:
    	-default webpage for thresholding upload
    
    Ubuntu-B.ttf:
    	-font needed to label thresholded images
    
    
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/focal_adhesions/trunk@7699 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/webapp/cgi/thresh_testing.pl
+++ src/webapp/cgi/thresh_testing.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -wT
+#!/usr/bin/perl -w
 
 use strict;
 use File::Path;
@@ -7,16 +7,17 @@ use File::Spec::Functions;
 use File::Copy;
 use File::Temp qw(tempfile tempdir);
 use POSIX;
-use CGI;
-use CGI::Carp;
+use CGI::Pretty;
+use CGI::Carp qw(fatalsToBrowser);
 use IO::Handle;
 use Config::General;
 use Data::Dumper;
+use HTML::Template;
 
 use lib './';
 use webserver_funcs qw(print_html_end);
 
-my $start = time;
+my $upload_start = time;
 
 $| = 1;
 
@@ -55,106 +56,34 @@ if (defined $lightweight_fh) {
     close $fh;
     print "Done loading file.";
     chmod 0777, "$output_file" or die "$!";
-	
-	#place a file in the upload dir to let the worker script know that the
-	#upload is complete and then wait for the one of the last images to be
-	#written to disk
-	open OUTPUT, ">$dir/upload_done";
-	print OUTPUT "done";
-	close OUTPUT;
-	my $sleep_count = 0;
-	while (! -e "$dir/thumbs/focal_image_4.jpg") {
-		sleep 1;
-		$sleep_count++;
-		if ($sleep_count > (10*60)) {
-			die "Threshold processing on $dir took over ten minutes, exiting.";
-		}
-	}
+	my $upload_end = time;
 
-	#start a new CGI object to build the browsing page for the thresholded image
-	#set
-	my $output_cgi = CGI->new();
-	open OUTPUT, ">$dir/index.html";
-	select OUTPUT;
-
-	print OUTPUT $output_cgi->start_html(
-		-title=>'Focal Adhesion Analysis Server - Threshold Testing',
-		-style=>'/FA_webapp/css/screen.css');
-	print "<div class=\"container\">\n";
-	print $output_cgi->h1('Focal Adhesion Analysis Server - Threshold Testing');
-	print $output_cgi->p, "It took ", time - $start_time, 
-		" seconds to process your image.", $output_cgi->br;
+	my $start_process = time;
+	system("octave --eval \"build_thresholded_image_sets('$output_file')\" > /dev/null 2> /dev/null");
+	my $end_process = time;
 	
-	print $output_cgi->p(),"\n";
-	print "<a href='focal_image_norm.png'><img src='thumbs/focal_image_norm.jpg'></a>\n";
-	print "<a href='focal_image_1.png'><img src='thumbs/focal_image_1.jpg'></a>\n";
-	print "<a href='focal_image_1.5.png'><img src='thumbs/focal_image_1.5.jpg'></a>\n";
-	print "<a href='focal_image_2.png'><img src='thumbs/focal_image_2.jpg'></a>\n";
-	print "<a href='focal_image_2.5.png'><img src='thumbs/focal_image_2.5.jpg'></a>\n";
-	print "<a href='focal_image_3.png'><img src='thumbs/focal_image_3.jpg'></a>\n";
-	print "<a href='focal_image_3.5.png'><img src='thumbs/focal_image_3.5.jpg'></a>\n";
-	print "<a href='focal_image_4.png'><img src='thumbs/focal_image_4.jpg'></a>\n";
-	print_html_end($output_cgi);
+	my $template = HTML::Template->new(filename => 'template/threshold_results.tmpl');
+	$template->param(run_time => $end_process - $start_process,upload_time => $upload_end - $upload_start);
+	
+	open OUTPUT, ">$dir/index.html" or die $!;
+	print OUTPUT $template->output;
 	close OUTPUT;
 	
-	select STDOUT;
-
 	my $url_out_dir = $dir;
 	$url_out_dir =~ s/$ENV{'DOCUMENT_ROOT'}//g;
 	my $hostname = $ENV{'HTTP_ORIGIN'};
 	my $final_url = "$ENV{'HTTP_ORIGIN'}/$url_out_dir";
-	
+
 	print $q->redirect($final_url);
 } else {
     #no valid file handle was available, instead present the user with the
     #regular upload page
-	print $q->header,
-		  $q->start_html(
-			  -title=>'Focal Adhesion Analysis Server - Threshold Testing',
-			  -style=>'/FA_webapp/css/screen.css');
-
-	print "<div class=\"container\">\n";
-	print $q->h1('Focal Adhesion Analysis Server - Threshold Testing');
-
-    print $q->start_form(-method=>"POST",
-                     -action=>"thresh_testing.pl",
-                     -enctype=>"multipart/form-data");
-    print $q->h3('Adhesion Image File'), 
-          $q->filefield('uploaded_file','',50,80);
-    
-    print $q->br,$q->br,
-          $q->submit(-name=>"Submit Data");
-
-    print $q->end_form;
-
-    print $q->br,$q->hr;
-
-	print $q->p, "The most critical parameter in the analysis pipeline is the
-	threshold used to segment the focal adhesions from background. Since running
-	the full analysis can take several hours, you may want to run a
-	representative image through this system to see what threshold will be most
-	appropriate for your data set. It might take up to 30 seconds depending on
-	the server load for an image to be uploaded and processed. After the image
-	processed you will be automatically forwarded to the results page.";
-
-    print $q->br;
-    print $q->br;
+	my $template = HTML::Template->new(filename => 'template/threshold_start.tmpl');
 	
-	print $q->p, "Most image formats should work in the thresholding algorithm,
-	but I've only tested it with tiff (unstacked please) and png files. Also
-	keep in mind that in a full run of the processing pipeline, the entire image
-	set is used to determine the actual threshold value, here only the image you
-	provide is used. Thus, there will be some slight differences in the exact
-	threshold selected, but in general I've found them to be very similar.";
-	
-	print $q->br,$q->br, "Don't have an image to upload as a test? Try this ", 
-		$q->a({href=>"/FA_webapp/sample/sample_image.png"},'one'), ".";
-	
-	print $q->br,$q->br, "See a sample result page ", 
-		$q->a({href=>"/FA_webapp/threshold_testing_sample"},'here'), ".";
+	print $q->header();
+	print $template->output;
 }
 
-print_html_end($q);
 
 ###############################################################################
 # Functions
