fcb1e490d0da9232b0bcdd0600f81b1c8d5ed557
mbergins@ISIS.UNC.EDU <mbergins@ISIS.UNC.EDU@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2013-01-21 15:46:49 +0000

    email_on_low_disk.pl: 	-changing threshold to 90% full
    setup_results_by_day.pl:
    	-new script to organize experiments/data sets by day
    
    
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/focal_adhesions/trunk@7785 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/webapp/email_on_low_disk.pl
+++ src/webapp/email_on_low_disk.pl
@@ -4,7 +4,7 @@ my @df_lines = split(/\n/,`df -h 2>/dev/null`);
 
 for (@df_lines) {
 	if ($_ =~ /sda1.*(\d+)%/) {
-		if ($1 > 4) {
+		if ($1 > 90) {
 			system("echo \"$_\" | mail -s \"Disk Usage Hit\" matthew.berginski\@gmail.com");
 		}
 	}
