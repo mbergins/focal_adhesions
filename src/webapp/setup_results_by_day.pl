67e4011b0152d0ea111eb48193758b1e94d09909
mbergins@ISIS.UNC.EDU <mbergins@ISIS.UNC.EDU@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2013-01-30 20:37:23 +0000

    setup_results_by_day.pl: 	-adding setup of year and month folders as well
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/focal_adhesions/trunk@7807 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/webapp/setup_results_by_day.pl
+++ src/webapp/setup_results_by_day.pl
@@ -14,7 +14,14 @@ foreach (@results_folders) {
 	my $date = &POSIX::strftime("%Y-%m-%d", localtime( ( stat $_ )[9]));
 	mkpath("$target_folder/$date");
 	my $abs_target = &File::Spec::Functions::rel2abs($_);
-
+	system("ln -s $abs_target $target_folder/$date");
+	
+	$date = &POSIX::strftime("%Y-%m", localtime( ( stat $_ )[9]));
+	mkpath("$target_folder/$date");
+	system("ln -s $abs_target $target_folder/$date");
+	
+	$date = &POSIX::strftime("%Y", localtime( ( stat $_ )[9]));
+	mkpath("$target_folder/$date");
 	system("ln -s $abs_target $target_folder/$date");
 }
 
@@ -32,7 +39,14 @@ foreach (@results_folders) {
 	my $date = &POSIX::strftime("%Y-%m-%d", localtime( ( stat $_ )[9]));
 	mkpath("$target_folder/$date");
 	my $abs_target = &File::Spec::Functions::rel2abs($_);
-
+	system("ln -s $abs_target $target_folder/$date");
+	
+	$date = &POSIX::strftime("%Y-%m", localtime( ( stat $_ )[9]));
+	mkpath("$target_folder/$date");
+	system("ln -s $abs_target $target_folder/$date");
+	
+	$date = &POSIX::strftime("%Y", localtime( ( stat $_ )[9]));
+	mkpath("$target_folder/$date");
 	system("ln -s $abs_target $target_folder/$date");
 }
 
