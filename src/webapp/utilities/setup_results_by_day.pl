1fe82a3f2af16145af586a72fd7e366933c72234
mbergins@ISIS.UNC.EDU <mbergins@ISIS.UNC.EDU@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2013-03-13 19:12:42 +0000

    setup_results_by_day.pl: 	-adding data by IP address folder
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/focal_adhesions/trunk@7914 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/webapp/utilities/setup_results_by_day.pl
+++ src/webapp/utilities/setup_results_by_day.pl
@@ -3,8 +3,11 @@
 use POSIX;
 use File::Path;
 use File::Spec::Functions;
+use Config::General;
 
-my $target_folder = "results_by_day";
+my $target_folder;
+
+$target_folder = "results_by_day";
 
 File::Path::remove_tree($target_folder);
 
@@ -32,9 +35,9 @@ $target_folder = "data_by_day";
 
 File::Path::remove_tree($target_folder);
 
-@results_folders = <../../../data/*>;
+@data_folders = <../../../data/*>;
 
-foreach (@results_folders) {
+foreach (@data_folders) {
 	next if ($_ =~ /config/);
 	my $date = &POSIX::strftime("%Y-%m-%d", localtime( ( stat $_ )[9]));
 	mkpath("$target_folder/$date");
@@ -50,3 +53,38 @@ foreach (@results_folders) {
 	system("ln -s $abs_target $target_folder/$date");
 }
 
+###########################################################
+# IP Address Folder Organization
+###########################################################
+&make_data_links_by_config_var("submitter_ip");
+
+###############################################################################
+# Functions
+###############################################################################
+
+sub make_data_links_by_config_var {
+	my $cfg_var = $_[0];
+	
+	my $target_folder = "data_by_$cfg_var";
+
+	File::Path::remove_tree($target_folder);
+
+	@data_folders = <../../../data/*>;
+
+	foreach (@data_folders) {
+		next if ($_ =~ /config/);
+		my @cfg = <$_/*.cfg>;
+		$conf = new Config::General((-ConfigFile => $cfg[0],
+				-IncludeRelative => 1));
+		my %config = $conf->getall;
+
+		my $abs_target = &File::Spec::Functions::rel2abs($_);
+		if (defined $config{$cfg_var}) {
+			mkpath("$target_folder/$config{$cfg_var}");
+			system("ln -s $abs_target $target_folder/$config{$cfg_var}");
+		} else {
+			mkpath("$target_folder/no_$cfg_var");
+			system("ln -s $abs_target $target_folder/no_$cfg_var");
+		}
+	}
+}
