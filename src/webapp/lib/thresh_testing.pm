99037fba77139dcec59ebb3c92ef26938381075b
mbergins@ISIS.UNC.EDU <mbergins@ISIS.UNC.EDU@ca063d7d-9bfa-4cd9-82ec-4107ad7834e0>
2013-03-27 14:57:21 +0000

    thresh_testing.pm: 	-removing upload time calculation, not accurate under fastcgi
    thresh_testing_results.tt:
    	-removing upload time display
    
    
    git-svn-id: https://svn-rc.unc.edu/gomez_lab/focal_adhesions/trunk@7946 ca063d7d-9bfa-4cd9-82ec-4107ad7834e0

--- src/webapp/lib/thresh_testing.pm
+++ src/webapp/lib/thresh_testing.pm
@@ -13,12 +13,10 @@ use File::Find;
 use Config::General;
 
 my $out_folder = catdir('threshold_testing');
-my $start_upload = time;
 
 ###############################################################################
 # Main
 ###############################################################################
-
 get '/thresh_testing' => sub {
 	template 'thresh_testing';
 };
@@ -34,16 +32,13 @@ post '/thresh_testing' => sub {
 	chmod 0777, $out_dir;
 	my $out_file = catfile($out_dir,"input");
 	$input_file->copy_to($out_file);
-	my $end_upload = time;
 
 	my $start_process = time;
 	system("octave --eval \"cd ../misc_code; build_thresholded_image_sets('../public/$out_file')\" > /dev/null 2> /dev/null");
 	my $end_process = time;
 	
-	my %cfg = (upload_time => $end_upload - $start_upload, 
-		process_time => $end_process - $start_process);
-
-	my $out_html = &generate_html(%cfg);
+	my $out_html = template 'thresh_testing_results', 
+		{process_time => $end_process - $start_process};
 	open OUTPUT, ">" . catfile($out_dir,'index.html');
 	print OUTPUT $out_html;
 	close OUTPUT;
@@ -54,10 +49,5 @@ post '/thresh_testing' => sub {
 ###############################################################################
 # Functions
 ###############################################################################
-
-sub generate_html {
-	my %cfg = @_;
-	return template 'thresh_testing_results', \%cfg;
-}
 
 true;
