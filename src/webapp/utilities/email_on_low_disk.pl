26f78e0126cb7df72bf34f7965ac49351efd8728
Matthew Berginski <matthew.berginski@gmail.com>
2018-06-13 11:50:23 -0400

    adding email cmd line option and fixing regex

--- src/webapp/utilities/email_on_low_disk.pl
+++ src/webapp/utilities/email_on_low_disk.pl
@@ -1,11 +1,17 @@
 #!/usr/bin/perl -w
 
+if (scalar(@ARGV) == 0) {
+	die "Expected one parameter: an email address";
+}
+
+my $email_address = $ARGV[0];
+
 my @df_lines = split(/\n/,`df -h 2>/dev/null`);
 
 for (@df_lines) {
-	if ($_ =~ /sda1.*(\d+)%/) {
+	if ($_ =~ /vg-root.*?(\d+)%/) {
 		if ($1 > 90) {
-			system("echo \"$_\" | mail -s \"Disk Usage Hit\" matthew.berginski\@gmail.com");
+			system("echo \"$_\" | mail -s \"Disk Usage Hit ($1%)\" $email_address");
 		}
 	}
 }
